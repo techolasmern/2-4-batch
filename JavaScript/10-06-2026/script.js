b5c27b7c867d7529c4ba1defc6084a3cf66f2538
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-10 14:43:56 +0530

    variable declare

---
+++ JavaScript/10-06-2026/script.js
@@ -0,0 +1,22 @@
+// variable declaration
+
+// var a;
+// let b;
+// const b;
+
+var a = 25;
+console.log(a);
+var a = 30;
+console.log(a);
+a = 45;
+console.log(a);
+// var -> re-declare is allowed & re-assign is allowed
+
+let b = 35;
+console.log(b);
+b = 40;
+console.log(b);
+// var -> re-declare is not allowed & re-assign is allowed
+
+const pi = 3.14;
+// const -> re-declare is not allowed & re-assign is not allowed
