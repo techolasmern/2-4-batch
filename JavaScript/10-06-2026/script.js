c7fa516e94e0bbff448080c844cd7df609e3b68c
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-10 15:04:50 +0530

    operators - 1

--- JavaScript/10-06-2026/script.js
+++ JavaScript/10-06-2026/script.js
@@ -20,3 +20,40 @@ console.log(b);
 
 const pi = 3.14;
 // const -> re-declare is not allowed & re-assign is not allowed
+
+// Operators
+
+// Assignment Operator,  Arithmatic, Comparison, Unary Operator
+
+let num = 12;
+// +=, -=, *=, /=
+
+// +, -, *, /, %, **
+
+console.log('--------------------------------------------------')
+
+console.log(1 + 1);
+console.log("1" + "1");
+console.log("1" + 1);
+console.log(1 + "1");
+
+// console.log(1 + +1); 
+console.log(1 + +"1"); 
+
+console.log(10 - 5);
+console.log("10" - 5)
+
+console.log(10 * 2)
+console.log(10 / 2)
+console.log(10 ** 3)
+console.log(10 % 4);
+
+let num1 = 10;
+let num2 = 20;
+
+num1 = num1 + num2; // a = a + b -> a += b;
+num1 = num1 - num2; // a = a - b -> a -= b;
+num1 = num1 * num2; // a = a * b -> a *= b;
+num1 = num1 / num2; // a = a / b -> a /= b;
+
+console.log(num1);
