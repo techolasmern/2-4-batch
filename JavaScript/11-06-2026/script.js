81865a9e94bb9eb77d69457ccc9ce71382982e42
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-11 15:38:46 +0530

    button click - function

--- JavaScript/11-06-2026/script.js
+++ JavaScript/11-06-2026/script.js
@@ -1,60 +1,72 @@
-// Function -> A set of instruction or block of code to perform a specific task.
-// re-usable
+// // Function -> A set of instruction or block of code to perform a specific task.
+// // re-usable
 
-// function <fnId>() {
+// // function <fnId>() {
     
+// // }
+
+// function displayText(pin, n1, n2, n3) { // parameter
+//     console.log("The pin that you've entered is: " + pin)
+//     console.log(n1, n2, n3);
 // }
 
-function displayText(pin, n1, n2, n3) { // parameter
-    console.log("The pin that you've entered is: " + pin)
-    console.log(n1, n2, n3);
+// // function invoke / calling
+// displayText(4635, 10, 56, 87); // arguments
+
+// // get response from function
+
+// function sum(firstNumber, secondNumber, thirdNumber) {
+//     const result = firstNumber + secondNumber + thirdNumber;
+//     return result;
+// }
+// // console.log(result)
+// const res = sum(50, 100, 150);
+
+// // volume
+
+// function getArea(l, b) {
+//     const area = l * b;
+//     return area;
+// }
+
+// function getVolume(area, h) {
+//     const vol = area * h;
+//     return vol;
+// }
+
+// const ar = getArea(10, 20);
+// const v = getVolume(ar, 30);
+// console.log(v);
+
+// function sumOfNNumbers(limit) {
+//     const sum = (limit * (limit + 1)) / 2;
+//     return sum;
+// }
+
+// const r = sumOfNNumbers(100);
+// console.log(r);
+
+// // const enteredValue = prompt("Enter a value...");
+// // console.log(enteredValue);
+
+// // logical operators
+
+// // AND, OR, NOT
+// // &&, ||, !
+
+// // 0, null, undefind, ""
+// console.log(true && true && false && true); // => false;
+// console.log(false || false || true || false); // => true;
+// console.log(!false);
+
+function handleClick() {
+    console.log("Button clicked");
 }
 
-// function invoke / calling
-displayText(4635, 10, 56, 87); // arguments
+// add, sub, mul, div
+// ATM 
+let amountAvailble = 100000;
+// withdrawal - prompt();, display amount after withdrawal
+// deposit ->
 
-// get response from function
-
-function sum(firstNumber, secondNumber, thirdNumber) {
-    const result = firstNumber + secondNumber + thirdNumber;
-    return result;
-}
-// console.log(result)
-const res = sum(50, 100, 150);
-
-// volume
-
-function getArea(l, b) {
-    const area = l * b;
-    return area;
-}
-
-function getVolume(area, h) {
-    const vol = area * h;
-    return vol;
-}
-
-const ar = getArea(10, 20);
-const v = getVolume(ar, 30);
-console.log(v);
-
-function sumOfNNumbers(limit) {
-    const sum = (limit * (limit + 1)) / 2;
-    return sum;
-}
-
-const r = sumOfNNumbers(100);
-console.log(r);
-
-// const enteredValue = prompt("Enter a value...");
-// console.log(enteredValue);
-
-// logical operators
-
-// AND, OR, NOT
-// &&, ||, !
-
-// 0, null, undefind, ""
-console.log(true && true && false && true); // => false;
-console.log(false || false || true || false); // => true;
-console.log(!false);
+// sum of 3 numbers -> using prompt();
