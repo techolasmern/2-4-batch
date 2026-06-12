9a933e74cfb9ad0ca95b9ae5e85a8cf867239beb
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-12 14:34:20 +0530

    if, else, else if

---
+++ JavaScript/12-06-2026/script.js
@@ -0,0 +1,48 @@
+// alert("Hello");
+
+// const n1 = parseInt(prompt("Enter number 1:"))
+// const n2 = parseInt(prompt("Enter number 2:"))
+
+// const sum = n1 + n2;
+
+// alert("Sum of number: " + sum);
+
+// const number = "10.3"
+
+// console.log(Number(number));
+
+// conditional statement (control statement)
+
+// if, else, else if
+
+// console.log(10 < 20);
+
+// if (10 > 20) {
+//     // true statement
+//     console.log("10 is less than 20");
+// } else {
+//     // false statement
+//     console.log("10 is not greater than 20");
+// }
+
+const num1 = 20;
+const num2 = 30;
+
+if (num2 > num1) {
+    console.log(num2 + " is greater than " + num1);
+} else {
+    console.log(num1 + " is greater than " + num2);
+}
+
+const n1 = 100;
+const n2 = 200;
+const n3 = 300;
+
+if (n1 > n2 && n1 > n3) {
+    console.log("n1 is greater than n2, n3");
+} else if (n2 > n3) {
+    console.log("n2 is greater than n1, n3");
+} else {
+    console.log("n3 is greater than n1, n2");
+}
+
