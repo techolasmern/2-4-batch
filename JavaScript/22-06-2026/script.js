07c6216cdb65ed16a20b7340ad4de211d0424cec
VISHNU M K <techolas.vishnumk@gmail.com>
2026-06-22 15:55:08 +0530

    questions

--- JavaScript/22-06-2026/script.js
+++ JavaScript/22-06-2026/script.js
@@ -75,3 +75,31 @@ const arr = [1, 2, 3, 4, 5, 6, 7];
 arr.splice(2, 0, 30, 40, 50, 60, 70);
 
 console.log(arr);
+
+// number hide -> 9876543210 -> 98******10
+
+const hideNumber = (phone=0) => {
+    if (phone == 0) return "Please provider a valid number";
+    const toStr = phone.toString();
+    const firstDigits = toStr.slice(0, 2);
+    const lastDigits = toStr.slice(-2);
+    return firstDigits + "*****" + lastDigits;
+}
+
+console.log(hideNumber(9876543213));
+console.log(hideNumber(6453247658));
+
+// hide email
+// john123@gmail.com => joh******@gmail.com
+
+const hideMail = (email = "john123@gmail.com") => {
+    if (!email || !email.includes("@")) return "Provide valid email";
+    const splitted = email.split("@");
+    const username = splitted[0];
+    const domain = splitted[1];
+    const starAddedUsername = username.slice(0, -4) + "******";
+    return starAddedUsername + "@" + domain;
+}
+
+console.log(hideMail("aswin123@gmail.com"));
+console.log(hideMail("amayatk@yahoo.com"));
