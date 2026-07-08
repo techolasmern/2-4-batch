3ba188e4f26e84e72501af4051d54db0165bce5a
VISHNU M K <techolas.vishnumk@gmail.com>
2026-07-08 14:52:54 +0530

    form events

--- JavaScript/08-07-2026/script.js
+++ JavaScript/08-07-2026/script.js
@@ -1,29 +1,48 @@
 // 1. media
 // 2. form events
 
-const natureVideo = document.getElementById("natureVideo");
+// const natureVideo = document.getElementById("natureVideo");
 
-natureVideo.addEventListener("play", () => {
-    console.log("video started playing");
-});
+// natureVideo.addEventListener("play", () => {
+//     console.log("video started playing");
+// });
 
-natureVideo.addEventListener("pause", () => {
-    console.log("video paused");
-});
-const toggleBtn = document.getElementById("toggleBtn");
+// natureVideo.addEventListener("pause", () => {
+//     console.log("video paused");
+// });
+// const toggleBtn = document.getElementById("toggleBtn");
 
-natureVideo.addEventListener("ended", () => {
-    console.log("video ended");
-    toggleBtn.innerHTML = "Play";
+// natureVideo.addEventListener("ended", () => {
+//     console.log("video ended");
+//     toggleBtn.innerHTML = "Play";
+// })
+
+
+// toggleBtn.addEventListener("click", () => {
+//     if (natureVideo.paused) {
+//         natureVideo.play();
+//         toggleBtn.innerHTML = "Pause";
+//     } else {
+//         natureVideo.pause();
+//         toggleBtn.innerHTML = "Play";
+//     }
+// })
+
+// Form Events
+
+const loginForm = document.getElementById("loginForm");
+const usernameCounter = document.getElementById("usernameCounter");
+
+let counter = 0;
+loginForm.addEventListener("input", (event) => {
+    counter++;
+    usernameCounter.innerText = counter;
+    console.log(event.target.name, event.target.value);
 })
 
-
-toggleBtn.addEventListener("click", () => {
-    if (natureVideo.paused) {
-        natureVideo.play();
-        toggleBtn.innerHTML = "Pause";
-    } else {
-        natureVideo.pause();
-        toggleBtn.innerHTML = "Play";
-    }
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // const username = document.getElementById("username").value;
+    const username = document.login_form.username.value;
+    console.log(username);
 })
