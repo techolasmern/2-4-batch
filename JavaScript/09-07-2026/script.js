fe4b9288ff35c506c9e402bae947d879a5a64bd9
VISHNU M K <techolas.vishnumk@gmail.com>
2026-07-09 14:54:00 +0530

    send email - Sample with emailjs

---
+++ JavaScript/09-07-2026/script.js
@@ -0,0 +1,33 @@
+const emailjs_api = "https://api.emailjs.com/api/v1.0/email/send";
+
+const content_type = "application/json";
+
+const serviceId = "Your service ID";
+const templateId = "Your Template ID";
+const publicKey = "Your public key";
+
+const sendMail = async () => {
+    try {
+        const data = {
+            service_id: serviceId,
+            template_id: templateId,
+            user_id: publicKey,
+            template_params: {
+                name: "<replace the name variable>",
+                company_mail: "<replace the company mail variable>",
+                email: "<replace the email variable>"
+            }
+        }
+        const response = await fetch(emailjs_api, {
+            method: "POST",
+            headers: {
+                "Content-Type": content_type
+            },
+            body: JSON.stringify(data)
+        });
+        const res = await response.text();
+        return res == "OK";
+    } catch (err) {
+        return console.log(err);
+    }
+}
