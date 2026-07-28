3336ec9689bcf639d382fc869e11a9b0d6d69e8d
VISHNU M K <techolas.vishnumk@gmail.com>
2026-07-28 14:49:14 +0530

    manage multiple data

--- ReactJS/src/App.jsx
+++ ReactJS/src/App.jsx
@@ -5,16 +5,44 @@ import { useState } from "react";
 const App = () => {
 
     // array destructure
-    const [counter, setCounter] = useState(0);
+    const [counter, setCounter] = useState({ first_counter: 0, second_counter: 0 });
 
-    const handleClick = () => {
-        setCounter(counter + 1);
+    const handleFirstCounterClick = (operator) => {
+        if (operator == "+") {
+            setCounter((prevState) => {
+                return { ...prevState, first_counter: prevState.first_counter + 1 };
+            })
+        } else {
+            setCounter((prevState) => {
+                return { ...prevState, first_counter: prevState.first_counter - 1 };
+            })
+        }
+    }
+
+    const handleSecondCounterClick = (operator) => {
+        if (operator == "+") {
+            setCounter((prevState) => {
+                return { ...prevState, second_counter: prevState.second_counter + 1 };
+            })
+        } else {
+            setCounter((prevState) => {
+                return { ...prevState, second_counter: prevState.second_counter - 1 };
+            })
+        }
     }
 
     return <div>
         <h2>State Management</h2>
-        <h3>State Data: {counter}</h3>
-        <button onClick={handleClick}>Click Me</button>
+        <h3>First Counter: {counter.first_counter}</h3>
+        <h3>Second Counter: {counter.second_counter}</h3>
+        <div>
+            <button onClick={() => handleFirstCounterClick("+")}>First Counter +</button>
+            <button onClick={() => handleFirstCounterClick("-")}>First Counter -</button>
+        </div>
+        <div>
+            <button onClick={() => handleSecondCounterClick("+")}>Second Counter +</button>
+            <button onClick={() => handleSecondCounterClick("-")}>Second Counter -</button>
+        </div>
     </div>
 }
 
