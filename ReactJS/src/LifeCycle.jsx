0d7bfddfdb363eda73cdddcc37f1a35a4223137a
VISHNU M K <techolas.vishnumk@gmail.com>
2026-07-30 14:55:10 +0530

    timer and api call with use effect

--- ReactJS/src/LifeCycle.jsx
+++ ReactJS/src/LifeCycle.jsx
@@ -5,12 +5,15 @@ import { useEffect, useState } from "react"
 export const LifeCycle = () => {
 
     const [counter, setCounter] = useState(0);
-    const [counter2, setCounter2] = useState(0);
+    const [timer, setTimer] = useState(0);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        console.log("Component Mounted or Updated");
+        const interval = setInterval(() => {
+            setTimer((prevTimer) => prevTimer + 1);
+        }, 1000);
         return () => {
-            console.log("Component Unmounted");
+            clearInterval(interval);
         }
     }, [counter]);
 
@@ -18,12 +21,31 @@ export const LifeCycle = () => {
         setCounter(counter + 1);
     }
 
-    const handleIncrement2 = () => {
-        setCounter2(counter2 + 1);
+    const getProducts = async () => {
+        try {
+            const response = await fetch("https://fakestoreapi.com/products");
+            const data = await response.json()
+            setProducts(data);
+        } catch (err) {
+            return alert("Failed to fetch");
+        }
     }
+
+    useEffect(() => {
+        getProducts();
+    }, []);
 
     return <div>
         <button onClick={handleIncrement}>Click Me - {counter}</button>
-        <button onClick={handleIncrement2}>Click Me - {counter2}</button>
+        <p>Timer: {timer}</p>
+        <div>
+            {
+                products.map(product => {
+                    return <div key={product.id}>
+                        <h4>{product.title}</h4>
+                    </div>
+                })
+            }
+        </div>
     </div>
 }
